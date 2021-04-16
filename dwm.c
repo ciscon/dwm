751e562a662deff68cfefc996ffba4b00e4cf52b
ciscon <ciscon@nicotinelounge.com>
2021-04-16 17:46:16 -0400

    remove borders on single client

--- dwm.c
+++ dwm.c
@@ -371,6 +371,10 @@ applysizehints(Client *c, int *x, int *y, int *w, int *h, int interact)
 	int baseismin;
 	Monitor *m = c->mon;
 
+	// return 1 if layout is monocle
+	if (&monocle == c->mon->lt[c->mon->sellt]->arrange)
+		return 1;
+
 	/* set minimum possible */
 	*w = MAX(1, *w);
 	*h = MAX(1, *h);
@@ -1510,6 +1514,15 @@ resizeclient(Client *c, int x, int y, int w, int h)
 	c->oldw = c->w; c->w = wc.width = w;
 	c->oldh = c->h; c->h = wc.height = h;
 	wc.border_width = c->bw;
+	if ((&monocle == c->mon->lt[c->mon->sellt]->arrange)
+			&& (!c->isfloating)
+			|| ((nexttiled(c->mon->clients) == c
+			&& !nexttiled(c->next)
+			&& (&tile == c->mon->lt[c->mon->sellt]->arrange)))) {
+		wc.border_width = 0;
+		c->w = wc.width += c->bw * 2;
+		c->h = wc.height += c->bw * 2;
+	}
 	XConfigureWindow(dpy, c->win, CWX|CWY|CWWidth|CWHeight|CWBorderWidth, &wc);
 	configure(c);
 	XSync(dpy, False);
