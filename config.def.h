be9b6ac8773facda9894aa24899426f319940e3f
ciscon <ciscon@nicotinelounge.com>
2022-08-30 14:05:30 -0400

    termcmd

--- config.def.h
+++ config.def.h
@@ -89,7 +89,6 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
 static const char *termcmd[]  = { "terminal", NULL };
 static const char *browsercmd[]  = { "browser", NULL };
 static const char *browsercmdincognito[]  = { "browser", "--incognito", "--private-window" };
