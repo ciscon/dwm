6ae310a9599e71a47ced77575722de8070362ed0
ciscon <ciscon@nicotinelounge.com>
2023-02-25 13:56:16 -0500

    apply tag movement

--- config.def.h
+++ config.def.h
@@ -60,6 +60,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+#include "tagmovement.c"
+
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
@@ -85,6 +87,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_h,      x_prevtag,      {0} },
+	{ MODKEY|ShiftMask,             XK_l,      x_nexttag,      {0} },
+	{ MODKEY|ControlMask,           XK_h,      x_prevtag_all,  {0} },
+	{ MODKEY|ControlMask,           XK_l,      x_nexttag_all,  {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
