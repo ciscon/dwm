9240a495a6e0958b6c4efb3b6dc2466094bbf5df
ciscon <ciscon@nicotinelounge.com>
2023-02-25 14:04:39 -0500

    customize config

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -32,8 +32,18 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "workspacesclient",     NULL,       NULL,       0,            1,           -1 },
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "thunderbird",  NULL,       NULL,       1,       0,           1},
+	{ "skype",  NULL,       NULL,       1<<5,       0,           -1},
+	{ "discord",  NULL,       NULL,       1<<6,       0,           -1},
+	{ "Hexchat",  NULL,       NULL,      1<<7,       0,           -1},
+	{ "Deadbeef",  NULL,       NULL,       1<<8,       0,           -1},
+	{ "Audacious",  NULL,       NULL,       1<<8,       0,           -1},
+	{ "Slack",  NULL,       NULL,       1<<5,       0,           -1},
+	{ "google-chat-electron",  NULL,       NULL,       1<<5,       0,           -1},
+	{ "google-chat-linux",  NULL,       NULL,       1<<5,       0,           -1},
+	{ "Steam",  NULL,       NULL,       1<<4,       0,           -1}
 };
 
 /* layout(s) */
@@ -53,7 +63,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -66,7 +76,12 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "terminal", NULL };
+static const char *browsercmd[]  = { "browser", NULL };
+static const char *browsercmdincognito[]  = { "browser", "--incognito", "--private-window" };
+static const char *xscreensavercmd[]  = { "xscreensaver-command", "-activate", NULL };
+static const char *screenshotcmd[]  = { "screenshot", NULL };
+
 
 #include "tagmovement.c"
 
@@ -74,6 +89,9 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = screenshotcmd} },
+	{ MODKEY|ShiftMask,             XK_g,      spawn,          {.v = browsercmdincognito } },
+	{ MODKEY,                       XK_g,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
